d3f7e4dee7be0049d8fcd9c6db12ec385c856a6a
DapP256 <danielandres.perezperez@alumnos.uai.edu.ar>
2026-07-21 23:04:34 -0300

    feature/image-display: se comprimen  imagenes en formato webp

--- app/components/Header/HeaderLogo.tsx
+++ app/components/Header/HeaderLogo.tsx
@@ -9,7 +9,7 @@ export const HeaderLogo = ({ scrolled, onNav }: HeaderLogoProps) => (
     style={{ background: 'none', border: 'none' }}
   >
     <Image
-      src={scrolled ? '/logo_transparente_negro.png' : '/logo_transparente_blanco.png'}
+      src={scrolled ? '/logo_transparente_negro.webp' : '/logo_transparente_blanco.webp'}
       alt="Innovarq Logo"
       width={150}
       height={50}
